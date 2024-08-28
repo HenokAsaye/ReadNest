1c6573055a6fa405cf29fff1e78fefeb10de4d01
Henok Asaye <henokasaye77@gmail.com>
2024-08-28 21:46:04 +0300

    add validator to for users login and for manipulating book

---
+++ src/validators/authValidators.js
@@ -0,0 +1,30 @@
+import {check , validationReult} from "express-validator"
+
+export const validateRegister = [
+    check('username')
+        .notEmpty().withMessage('username is required')
+        .isLength({min:3}).withMessage("username must be atleast 3 charachters long"),
+    check('email')
+        .isEmail().withMessage('please provide a valid message'),
+    check('password')
+        .isLength({min:6}).withMessage('Password atleast must have 6 chracters long '),
+    (req,res,next) =>{
+        const errors = validationReult(req)
+        if(!errors.isEmpty()){
+            return res.status(400).json({errors:errors.array()})
+        }
+        next()
+    }
+        
+];
+export const validateLogin =[
+    check('password')
+        .isEmpty().withMessage('password is required!'),
+    (req,res,next) =>{
+        const errors = validationReult(req)
+        if(!errors.isEmpty()){
+            return res.status(400).json({errors:errors.array()});
+        }
+        next();
+    }
+];
