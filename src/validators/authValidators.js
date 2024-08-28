e29515b466e590cdb37683b09f5748a0328f5e46
HenokAsaye <henokasaye77@gmail.com>
2024-08-28 21:50:57 +0300

    Update authValidators.js

--- src/validators/authValidators.js
+++ src/validators/authValidators.js
@@ -13,7 +13,7 @@ export const validateRegister = [
         if(!errors.isEmpty()){
             return res.status(400).json({errors:errors.array()})
         }
-        next()
+        next();
     }
         
 ];
