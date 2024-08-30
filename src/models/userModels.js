005a824394a1e79577726c54317f5b4d656ab0be
abel <abelgetachew143@gmail.com>
2024-08-30 14:02:21 +0300

    added some fields on the book schema

--- src/models/userModels.js
+++ src/models/userModels.js
@@ -23,5 +23,5 @@ const userSchema = new mongoose.Schema({
     }
 });
 
-const user = mongoose.model('user', userSchema);
-export default user;
+const User = mongoose.model('user', userSchema);
+export default User;
