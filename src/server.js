9a49f0254feb471bfb8f645157f480d686f40745
abel <abelgetachew143@gmail.com>
2024-08-31 10:24:16 +0300

    bug fix in the server

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 import dotenv from "dotenv";
 dotenv.config();
-import express from "express";
+import app from "./app.js";
 import connectToDb from "./config/db.js";
 
-const app = express();
+
 const port = process.env.PORT || 3000;
 
 connectToDb().then(() => {
