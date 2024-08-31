bf4a9a41765216ea9949462ef57aa2b01ce7dec2
HenokAsaye <henokasaye77@gmail.com>
2024-08-31 23:38:31 +0300

    Delete src directory

--- src/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { signup,login } from "../controllers/authController.js";
-import {validateRegister, validateLogin} from "../validators/authValidators.js";
-
-const router=Router();
-router.post("/register",validateRegister,signup);
-router.post("/login",validateLogin,login);
-
-export default router
--- src/utils/jwt.js
+++
@@ -1,8 +0,0 @@
-import jwt from "jsonwebtoken";
-import dotenv from "dotenv";
-dotenv.config();
-
-export const generateAuthToken=(user)=>{
-      const token=jwt.sign({id:user._id},process.env.JWT_KEY);
-      return token;
-}
