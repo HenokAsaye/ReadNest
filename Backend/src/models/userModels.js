ff593fda5752510639dfb3842203219b18ac2b80
abel <abelgetachew143@gmail.com>
2024-09-01 22:06:30 +0300

    added preferredGenre field in the model

--- Backend/src/models/userModels.js
+++ Backend/src/models/userModels.js
@@ -24,6 +24,10 @@ const userSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Book'
     }],
+    preferredGenre:{
+        type:String,
+        required:true
+    }
 }, { timestamps: true });
 
 
